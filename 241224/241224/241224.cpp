4d1308d17a7e6b67d65b0966a2628d59769280e9
astr_stj <suktaej@gmail.com>
2024-12-24 10:57:20 +0900

    vector, iterator

---
+++ 241224/241224/241224.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+class CContainer
+{
+public:
+    CContainer() { std::cout << "constructor" << std::endl; }
+    CContainer(const CContainer&& Con) { mNum = Con.mNum; std::cout << "Copy" << std::endl; }
+    ~CContainer() { std::cout << "destroy" << std::endl; }
+    int mNum = 10;
+};
+
+int main()
+{
+    std::vector<int> vecInt;
+    //int type을 저장하는 vector
+    std::vector<CContainer> vecCon;
+
+    std::vector<int>::iterator iter;
+    //iterator 선언
+    
+    //vecInt.reserve(20);
+    //vecInt.resize(20);
+    std::cout << "처음 Capacity:" << vecInt.capacity() << std::endl;
+    for (int i = 0;i < 10;i++)
+    {
+        vecInt.push_back(i * 10);
+        std::cout << "size:" << vecInt.size();
+		std::cout << ", Capacity:" << vecInt.capacity() << std::endl;
+    }
+
+    std::vector<int>::iterator iterEnd = vecInt.end();
+    //for (iter = vecInt.begin();iter != vecInt.end();iter++) 
+    //vecInt.end()호출이 반복횟수 만큼 발생
+    for (iter = vecInt.begin();iter != iterEnd;iter++) 
+    {
+        std::cout << *iter << std::endl;
+    }
+
+    return 0;
+}
